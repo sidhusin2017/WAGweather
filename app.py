1e3c002bf73032f21c0ba814761071196b1eaecf
Siddarth <Siddarth.subramanian@walgreens.com>
2017-11-12 18:55:51 -0600

    Commit

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "handleAdd":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://40.121.149.23:9001/cce"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
